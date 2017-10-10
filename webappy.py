716a3c2f0d2fe3d7250411ff6b9032d41cf7294c
ekoorb <31251551+ekoorb@users.noreply.github.com>
2017-10-10 09:31:46 -0700

    Update webappy.py

--- webappy.py
+++ webappy.py
@@ -14,6 +14,18 @@ def render_page1():
 @app.route("/p2")
 def render_page2():
     return render_template("page2.html")
+
+@app.route("/response")
+def render_response():
+    name = request.args['color']
+    #The request obj. stores info about the request sent to the server. 
+    #The args is a multi dict,(like a doctionary) but it can have multiple values for the same key
+    #The info in args is visible in the url for the pages being requested (ex.../response?color=blue)
+    if name == 'lavender':
+        reply = "That's my favorite color, too"
+    else:
+            reply = "My favorite color is lavender."
+            return render_template('response.html', response = reply)
     
     
 if __name__=="__main__":
