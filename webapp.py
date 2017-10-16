71e14a375c36356dc2fcbe3c852c480e5d221d5a
ekoorb <31251551+ekoorb@users.noreply.github.com>
2017-10-16 09:30:20 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,7 +3,7 @@ from flask import Flask, url_for, render_template, request
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
-def render_home():
+def render_main():
     if 'symbol' in request.args
     #name = request.args['symbol']
     #The request obj. stores info about the request sent to the server. 
