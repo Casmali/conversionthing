3b2a701f22443a16f36501cc514de3191c9c76ef
ekoorb <31251551+ekoorb@users.noreply.github.com>
2017-10-19 08:54:18 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 @app.route("/")
 def render_main():
     reply=""
-    if 'symbol' in request.args:
+        if 'symbol' in request.args:
     name = request.args['symbol']
         
         if name == 'sodium':
@@ -21,7 +21,7 @@ def render_main():
 @app.route("/page1")
 def render_page1():
     reply=""
-    if 'symbol' in request.args:
+        if 'symbol' in request.args:
     name = request.args['symbol']
     
          if name == 'gold':
@@ -38,7 +38,7 @@ def render_page1():
 @app.route("/page2")
 def render_page2():
     reply=""
-    if 'symbol' in request.args:
+        if 'symbol' in request.args:
     name = request.args['symbol']
     
          if name == 'carbon':
